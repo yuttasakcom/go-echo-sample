d8f9679a2b3547d427b101afac5d633f54501f69
Yuttasak(Yo)Pannawat <yuttasak.pan.true@gmail.com>
2023-07-08 22:26:35 +0700

    Create main.go

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 )
+
+type Movie struct {
+	ImdbID      string `json:"imdbID"`
+	Title       string `json:"Title"`
+	Year        string `json:"Year"`
+	Ratting     string `json:"ratting"`
+	IsSuperHero string `json:"isSuperHero"`
+}
+
+var movies = []Movie{}
+
+func getAllMovies(c echo.Context) error {
+	return c.JSON(http.StatusOK, movies)
+}
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello, Echo👋")
-	})
+	e.GET("/api/movies", getAllMovies)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
